002c67d25579917ae005fd13adb1d2e54a6eb521
Edwin Fuquen <efuquen@gmail.com>
2021-08-08 12:55:46 -0600

    Deploys frontend to Kubernetes.

--- ops/kubernetes.tf
+++ ops/kubernetes.tf
@@ -30,3 +30,70 @@ module "kubernetes_dashboard" {
   kubernetes_namespace_create = true
   kubernetes_dashboard_csrf = "${random_string.csrf_token.result}"
 }
+
+resource "kubernetes_deployment" "frontend" {
+  metadata {
+    name = "speech2text-frontend"
+    labels = {
+      App = "Speech2textFrontend"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        App = "Speech2textFrontend"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "Speech2textFrontend"
+        }
+      }
+      spec {
+        container {
+          image = "efuquen/speech2text-frontend:latest"
+          name  = "speech2text-frontend"
+
+          port {
+            container_port = 1323
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "frontend" {
+  metadata {
+    name = "speech2text-frontend"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.frontend.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 1323
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.frontend.status.0.load_balancer.0.ingress.0.hostname
+}
